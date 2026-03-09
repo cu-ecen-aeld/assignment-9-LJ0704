c01df698514156cd8d04bff218202a4d1bef797a
lijo6730 <lijo6730@likhita.myguest.virtualbox.org>
2026-03-08 20:09:20 -0600

    Full test is not working

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,30 +1,38 @@
-
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# AESD-ASSIGNMENTS PACKAGE FOR BUILDROOT
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '66273650a5aad7c7c398916ca2b95240336c1304'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
+AESD_ASSIGNMENTS_VERSION = ab0928c7ab7b677056a3fb2efab2c0996f728954
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-LJ0704.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+# This is the directory where your kernel module resides
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+# Build commands for userspace programs
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	# Build server and kernel module
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# Install userspace executables and kernel module helpers
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	# Install the executable to /usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-	
-	# Install the start-stop script to /etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+    # Install the socket server
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
+    # Install kernel module load/unload scripts
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+
+    # Ensure kernel module loads at boot
+    mkdir -p $(TARGET_DIR)/etc/modules-load.d
+    echo "aesdchar" > $(TARGET_DIR)/etc/modules-load.d/aesdchar.conf
 endef
 
+# First build kernel module, then install everything else
+$(eval $(kernel-module))
 $(eval $(generic-package))
