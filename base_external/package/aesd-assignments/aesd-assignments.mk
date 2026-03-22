0c5fe7d0766449ce810134dee504d42ea934ea7e
Likhita Jonnakuti <94250445+LJ0704@users.noreply.github.com>
2026-03-21 23:34:18 -0600

    Update AESD assignments version to b2b98fe

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 #ChatGPT : https://chatgpt.com/share/69ae35ac-a2fc-8009-92ab-f80581fea5c9
-AESD_ASSIGNMENTS_VERSION = b4dda09b512ca255a2d485a4c8d62d7bbbbd7d7e
+AESD_ASSIGNMENTS_VERSION = b2b98feadb4f6e4fd2c5a850dcca646f1cde0091
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-LJ0704.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
